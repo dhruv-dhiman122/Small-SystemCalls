627759d80affe27607f3ba6a85135c4cbb34ae4b
Dhruv Dhiman <dhruvdhiman770@gmail.com>
2026-02-05 10:20:42 +0530

    Created the Mount function and added the code for MS_BIND mount

--- mount_usb.c
+++ mount_usb.c
@@ -12,9 +12,27 @@
 #define EXT_SUCCESS 0
 #define EXT_FAILURE 1
 
-void MountSUB() {
+void Mount() {
   const char* mountPoint = "/mnt/usb-sandisk";
   const char* device = "/dev/sdb1";
+  
+  //creating the mounting point
+  if(mount(mountPoint, 0700) == 0) {
+    printf("Mounting point is created without prooblem\n");
+  }
+  else {
+    printf("Failed to create the mounting point\n");
+    exit("EXT_FAILURE");
+  }
+
+  //mounting the USB to the point
+  if(mount(mountPoint, device, NULL, MS_BIND, NULL) == 0) {
+    printf("Created a bind mount point\n");
+  }
+  else {
+    printf("Failed to create a bind mount point\n");
+    exit(EXT_FAILURE);
+  }
 }
 
 int main() {
